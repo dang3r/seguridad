a12f17952ad42d8e2b45bca6c1c5adb31d22ce2d
Daniel Cardoza <daniel@aerofs.com>
2017-08-30 19:46:12 -0700

    - make the api more friendly - override the to_s method instead of using .print

--- lib/seguridad.rb
+++ lib/seguridad.rb
@@ -4,32 +4,33 @@ require 'OpenSSL'
 require 'socket'
 
 class OpenSSL::X509::Certificate
-    # TODO : Fix
+    attr_accessor :domain
+
     def to_s
-    end
-    def print
-        puts "\tName: #{subject.to_s.match(/\/CN=[^\s]*/).to_s.split("=")[1]}"
-        puts "\tIssuer: #{issuer}"
-        puts "\tNot Before: #{not_before}"
-        puts "\tNot After: #{not_after}"
-        puts "\tVersion: #{version}"
+        [
+            domain,
+            "\tName: #{subject.to_s.match(/\/CN=[^\s]*/).to_s.split("=")[1]}",
+            "\tIssuer: #{issuer}",
+            "\tNot Before: #{not_before}",
+            "\tNot After: #{not_after}",
+            "\tVersion: #{version}"
+        ].join("\n")
     end
 end
 
+# Return a monkey-patched X509 certificate for the given domain
 def get_cert_info(domain)
-    puts domain
     port = 443
     tokens = domain.split(':')
     if tokens.length > 1
         domain = tokens[0]
         port = tokens[-1].to_i
     end
-    ctx = OpenSSL::SSL::SSLContext.new
     socket = TCPSocket.new(domain, port)
-
+    ctx = OpenSSL::SSL::SSLContext.new
     tls = OpenSSL::SSL::SSLSocket.new(socket, ctx)
     tls.connect
     cert = OpenSSL::X509::Certificate.new tls.peer_cert
-    cert.print
-    puts
+    cert.domain = domain
+    cert
 end
--- seguridad.gemspec
+++ seguridad.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
     s.name        = 'seguridad'
-    s.version     = '0.0.2'
+    s.version     = '0.0.3'
     s.executables << 'seguridad'
     s.date        = '2017-08-30'
     s.summary     = "Provides information about TLS certificates"
